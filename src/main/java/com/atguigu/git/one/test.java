ac42e34826fa1aea2b737c42a78b629edceeaf20
ZhangJY <1844897920@qq.com>
2022-04-26 14:20:32 +0800

    master master master

--- src/main/java/com/atguigu/git/one/test.java
+++ src/main/java/com/atguigu/git/one/test.java
@@ -4,5 +4,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("git test");
         System.out.println("second");
+        System.out.println("master master master");
     }
 }
